41fe092fa5d5b1c7d25d378be18ba1e992edcba0
bigzhe <imhaozhe@gmail.com>
2020-07-21 01:13:05 +0800

    removed the unrelated tabs from the home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,43 +1,20 @@
 <template>
   <div style="margin-top: 00px" class="container-fluid">
     <Tabs @on-click="handleClick" style="margin-bottom: 0;">
-        <TabPane label="Input" name="dataset"></TabPane>
-      <TabPane label="Model Selection" name="modelSelection"></TabPane>
-      <TabPane label="Regression" name="regression"></TabPane>
-      <TabPane label="Chow-Liu Tree" name="clt"></TabPane>
-      <TabPane label="Strategy" name="strategy"></TabPane>
+      <TabPane label="Input" name="dataset"></TabPane>
+      <TabPane label="View Generation" name="viewGeneration"></TabPane>
+      <TabPane label="View Groups" name="viewGroups"></TabPane>
+      <TabPane label="Code Generation" name="codeGeneration"></TabPane>
+      <TabPane label="Application" name="application"></TabPane>
     </Tabs>
     <br />
 
-    <!-- Control panel -->
-    <div v-if="tab !== 'dataset' && tab !== 'strategy' && tab !== 'about'">
-      <Button @click="nextBatch(true)">Process 1000 Updates</Button>
-      <!-- Processing Tuples: -->
-      <RadioGroup
-        style="margin-left: 5px"
-        @on-change="statusChange"
-        v-model="processingStatus"
-        type="button"
-      >
-        <Radio label="play">
-          <Icon type="md-play" />
-        </Radio>
-        <Radio label="pause">
-          <Icon type="md-pause" />
-        </Radio>
-      </RadioGroup>
-
-      <span style="margin-left: 10px">Processed {{tN}} tuples</span>
-      <br />
-      <br>
-    </div>
-
-    <!-- <About ref="about" v-if="tab == 'about'"></About> -->
-    <ModelSelection ref="modelSelection" v-if="tab == 'modelSelection'"></ModelSelection>
+    <Dataset ref="dataset"></Dataset>
+    <!-- <ModelSelection ref="modelSelection" v-if="tab == 'modelSelection'"></ModelSelection>
     <Regression ref="regression" v-else-if="tab == 'regression'"></Regression>
     <ChowLiuTree ref="clt" v-else-if="tab == 'clt'"></ChowLiuTree>
     <StaticStrategy ref="strategy" v-else-if="tab == 'strategy'"></StaticStrategy>
-    <Dataset ref="dataset" v-else-if="tab == 'dataset'"></Dataset>
+    <Dataset ref="dataset" v-else-if="tab == 'dataset'"></Dataset> -->
 
     <br />
     <br />
@@ -50,12 +27,12 @@
 
 <script>
 import Dataset from "../components/Dataset.vue";
-import ChowLiuTree from "../components/ChowLiuTree";
-import ComputationalModel from "../components/ComputationalModel";
-import ModelSelection from "../components/ModelSelection";
-import Regression from "../components/Regression";
-import StaticStrategy from "../components/StaticStrategy";
-import About from "../components/About";
+// import ChowLiuTree from "../components/ChowLiuTree";
+// import ComputationalModel from "../components/ComputationalModel";
+// import ModelSelection from "../components/ModelSelection";
+// import Regression from "../components/Regression";
+// import StaticStrategy from "../components/StaticStrategy";
+// import About from "../components/About";
 
 export default {
   name: "home",
@@ -69,65 +46,24 @@ export default {
   },
   components: {
     Dataset,
-    StaticStrategy,
-    ModelSelection,
-    ChowLiuTree,
-    Regression,
+    // StaticStrategy,
+    // ModelSelection,
+    // ChowLiuTree,
+    // Regression,
   },
   mounted: function() {
-    const self = this;
-    this.$store.dispatch('processNextBatch', function() {
-      // after fetched
 
-      self.$store.commit("SET_TAB", "clt");    
-
-    })
   },
   methods: {
     handleClick(newTab) {
       // console.log(newTab)
       this.$store.commit("SET_TAB", newTab);
     },
-    nextBatch(force) {
-      const self = this;
-      this.$store.dispatch("processNextBatch", function() {
-        // after fetched
-
-        if (force || self.previousCount !== self.$store.state.count) {
-          self.$refs[self.tab].renderD3();
-          self.previousCount = self.$store.state.count;
-          console.log(`Count: ${self.$store.state.count}`);
-        }
-
-        // self.$Message.success(`Count: ${self.$store.state.count}`)
-      });
-    },
-    statusChange(status) {
-      if (status === "play") {
-        this.startProcessing();
-      } else {
-        this.pauseProcessing();
-      }
-    },
-    startProcessing() {
-      const self = this;
-      this.interval = setInterval(() => {
-        self.nextBatch();
-      }, 250);
-      self.$store.commit("SET_PROCESSING", true);
-    },
-    pauseProcessing() {
-      clearInterval(this.interval);
-      this.$store.commit("SET_PROCESSING", false);
-    }
   },
   computed: {
     tab: function() {
       return this.$store.state.tab;
     },
-    tN: function() {
-      return this.$store.state.tN;
-    }
   }
 };
 </script>
