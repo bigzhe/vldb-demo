27775d5a8497fd40f8f9a30a4d3f114cf73a5aa3
bigzhe <imhaozhe@gmail.com>
2020-07-30 19:56:17 +0800

    change the language of vuejs to English

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,8 +4,9 @@ import router from './router'
 import store from './store'
 import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
+import locale from 'view-design/dist/locale/en-US';
 
-Vue.use(ViewUI);
+Vue.use(ViewUI, {locale});
 
 Vue.config.productionTip = false
 
